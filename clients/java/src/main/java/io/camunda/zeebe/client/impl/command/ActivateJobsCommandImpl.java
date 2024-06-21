40a0a759ef9ec02f9aee869f5e4446f4401d86cf
Nicola Puppa <nicola.puppa@camunda.com>
2024-06-21 13:51:08 +0200

    refactor: deprecate old send final step command in favor of new command

--- clients/java/src/main/java/io/camunda/zeebe/client/impl/command/ActivateJobsCommandImpl.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/impl/command/ActivateJobsCommandImpl.java
@@ -15,8 +15,10 @@
  */
 package io.camunda.zeebe.client.impl.command;
 
+import io.camunda.zeebe.client.CamundaClientConfiguration;
 import io.camunda.zeebe.client.CredentialsProvider.StatusCode;
 import io.camunda.zeebe.client.ZeebeClientConfiguration;
+import io.camunda.zeebe.client.api.CamundaFuture;
 import io.camunda.zeebe.client.api.JsonMapper;
 import io.camunda.zeebe.client.api.ZeebeFuture;
 import io.camunda.zeebe.client.api.command.ActivateJobsCommandStep1;
@@ -52,6 +54,23 @@ public final class ActivateJobsCommandImpl
   private final Set<String> defaultTenantIds;
   private final Set<String> customTenantIds;
 
+  public ActivateJobsCommandImpl(
+      final GatewayStub asyncStub,
+      final CamundaClientConfiguration config,
+      final JsonMapper jsonMapper,
+      final Predicate<StatusCode> retryPredicate) {
+    this.asyncStub = asyncStub;
+    this.jsonMapper = jsonMapper;
+    this.retryPredicate = retryPredicate;
+    builder = ActivateJobsRequest.newBuilder();
+    requestTimeout(config.getDefaultRequestTimeout());
+    timeout(config.getDefaultJobTimeout());
+    workerName(config.getDefaultJobWorkerName());
+    defaultTenantIds = new HashSet<>(config.getDefaultJobWorkerTenantIds());
+    customTenantIds = new HashSet<>();
+  }
+
+  @Deprecated
   public ActivateJobsCommandImpl(
       final GatewayStub asyncStub,
       final ZeebeClientConfiguration config,
@@ -113,7 +132,32 @@ public final class ActivateJobsCommandImpl
   }
 
   @Override
+  @Deprecated
   public ZeebeFuture<ActivateJobsResponse> send() {
+    builder.clearTenantIds();
+    if (customTenantIds.isEmpty()) {
+      builder.addAllTenantIds(defaultTenantIds);
+    } else {
+      builder.addAllTenantIds(customTenantIds);
+    }
+
+    final ActivateJobsRequest request = builder.build();
+
+    final ActivateJobsResponseImpl response = new ActivateJobsResponseImpl(jsonMapper);
+    final RetriableStreamingFutureImpl<ActivateJobsResponse, GatewayOuterClass.ActivateJobsResponse>
+        future =
+            new RetriableStreamingFutureImpl<>(
+                response,
+                response::addResponse,
+                retryPredicate,
+                streamObserver -> send(request, streamObserver));
+
+    send(request, future);
+    return future;
+  }
+
+  @Override
+  public CamundaFuture<ActivateJobsResponse> sendCommand() {
     builder.clearTenantIds();
     if (customTenantIds.isEmpty()) {
       builder.addAllTenantIds(defaultTenantIds);
