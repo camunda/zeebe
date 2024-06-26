501e165c0852ca054ef2bfcb07dac3873e44b76f
PHWaechtler <helene.waechtler@camunda.com>
2024-06-26 16:49:32 +0200

    ci: comment out C7 rest IT
    related to OPT #13300 part 2

--- optimize/backend/src/it/java/io/camunda/optimize/jetty/SpringBootCompressionIT.java
+++ optimize/backend/src/it/java/io/camunda/optimize/jetty/SpringBootCompressionIT.java
@@ -7,23 +7,24 @@
  */
 // TODO recreate C8 IT equivalent of this with #13337
 
-//package io.camunda.optimize.jetty;
+// package io.camunda.optimize.jetty;
 //
-//import static io.camunda.optimize.AbstractIT.OPENSEARCH_PASSING;
+// import static io.camunda.optimize.AbstractIT.OPENSEARCH_PASSING;
 //
-//import io.camunda.optimize.AbstractPlatformIT;
-//import java.util.Collections;
-//import org.junit.jupiter.api.Tag;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.test.annotation.DirtiesContext;
+// import io.camunda.optimize.AbstractPlatformIT;
+// import java.util.Collections;
+// import org.junit.jupiter.api.Tag;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.test.annotation.DirtiesContext;
 //
-//@DirtiesContext
-//@Tag(OPENSEARCH_PASSING)
-//public class SpringBootCompressionIT extends AbstractPlatformIT {
+// @DirtiesContext
+// @Tag(OPENSEARCH_PASSING)
+// public class SpringBootCompressionIT extends AbstractPlatformIT {
 //
 //  @Test
 //  public void optimizeBootsWithEnabledCompression() {
-//    startAndUseNewOptimizeInstance(Collections.singletonMap("server.compression.enabled", "true"));
+//    startAndUseNewOptimizeInstance(Collections.singletonMap("server.compression.enabled",
+// "true"));
 //
 //    embeddedOptimizeExtension
 //        .getRequestExecutor()
@@ -31,4 +32,4 @@
 //        .execute(200)
 //        .close();
 //  }
-//}
+// }
